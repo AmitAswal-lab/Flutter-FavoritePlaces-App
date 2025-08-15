33e522dc82dd128ee3b111cd0209865ffac137ee
Amit Aswal <gametheory284@gmail.com>
2025-08-15 20:03:12 +0530

    integrated Google Maps with location selection, preview, and place details

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -3,12 +3,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'dart:io';
 
-class UserPlacesNotifier extends StateNotifier<List<Place> >{
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image){
-    final newPlace = Place(title: title,image: image);
-    state = [newPlace ,...state,];
+  void addPlace(String title, File image, PlaceLocation location) {
+           final newPlace = Place(title: title, image: image, location: location);
+
+
+    state = [newPlace, ...state];
   }
 }
 
